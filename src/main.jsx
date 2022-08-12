40a90b1f9af33f30a5c0e843a67c9d8d4e867bb5
Ana <amarcosestevez@gmail.com>
2022-08-12 14:34:58 +0200

    add solved task

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import './styles/main.css';
-import { HooksApp } from './HooksApp';
+
+import { CounterApp } from './01-useState/CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <HooksApp />
+ <CounterApp />
   </React.StrictMode>
 )
