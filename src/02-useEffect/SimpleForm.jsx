8068f34cc4f16ce4e559033aa130397acacce2f7
Ana <amarcosestevez@gmail.com>
2022-08-12 16:49:54 +0200

    add an example to see how the useEffect works by removing components when they aren't needed

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,36 +1,55 @@
 
-
+import { useState, useEffect } from "react";
+import { Message } from "./Message";
 
 export const SimpleForm = () => {
 
    const [formState, setFormState] = useState({
-    username: 'Ana',
-    email: 'aimarest@google.com',
+    username: '',
+    email: '',
    }); 
 
-const { username, email } = formState;
-const onInputChange = (event) =>{
+    const { username, email } = formState;
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
 
-    setFormState(event.target.value)
-}
+        setFormState({ ...formState, [name]: value })
+    }
+
+    useEffect(() => {
+       console.log('useEffect called!')
+    }, []);
+    useEffect(() => {
+        console.log('email changed!')
+     }, [ email ]);
 
   return (
  <>
     <h1>Simple Form</h1>
     <form>
-        <input type="text"
-        className="form-control"
-        placeholder="Username"
-        name="username"
-        value={ username }
-        onChange={ onInputChange }></input>
-          <input type="email"
-        className="form-control mt-2"
-        placeholder="aimarest@gmail.com"
-        name="email"
-        value={ email }
-        onChange={ onInputChange }></input>
+              <input type="text"
+                  className="form-control"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={onInputChange}>
+
+              </input>
+
+              <input type="email"
+                  className="form-control mt-2"
+                  placeholder="aimarest@gmail.com"
+                  name="email"
+                  value={email}
+                  onChange={onInputChange}>
+
+              </input>
     </form>
+
+    {
+       ( username === 'Ana')&&  <Message />
+    }
+   
  </>
   )
 }
