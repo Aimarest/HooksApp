375c418ae6342ad1cc13f2176903dfab21b8f6de
Ana <amarcosestevez@gmail.com>
2022-08-22 17:07:42 +0200

    add useLayoutEffect

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,32 @@
+import { useFetch, useCounter } from "../Hooks";
+import { LoadingQuote, Quote } from "../03-examples";
+
+
+export const Layout = () => {
+
+    const { onAdd, onSubtract, counter } = useCounter();
+    const { data, isLoading, hasError } = useFetch(` https://www.breakingbadapi.com/api/quotes/${ counter } `)
+    const { author, quote } = !!data && data[0];  // Esto lo que hace es que coge la posición 0 del array data si data no es nulo.
+   
+
+  return (
+    <>
+        <h1>BreakingBad Quotes</h1>
+        <hr />
+        {
+
+            isLoading
+                ? <LoadingQuote />
+                :  <Quote author={ author } quote={ quote }/>
+                
+        }
+
+       <button onClick={ onAdd } className="btn btn-primary" disabled={ isLoading }>
+        Next quote
+       </button>
+       <button onClick={ onSubtract } className="btn btn-primary" disabled={ isLoading } >
+        Previous quote
+       </button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { FocusScreen } from './04-useRef/FocusScreen';
+
+import { Layout } from './05-useLayoutEffect/Layout';
 import './styles/main.css';
 
 
@@ -9,9 +10,10 @@ import './styles/main.css';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'; 
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+//import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <FocusScreen  />
+ <Layout />
   </React.StrictMode>
 )
