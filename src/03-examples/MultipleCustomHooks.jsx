dc7561cd880d0c8368e671bd02a85d243bdcbad0
Ana <amarcosestevez@gmail.com>
2022-08-22 12:57:01 +0200

    add desestructuring to data to return html after loading

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,11 +4,32 @@ import { useFetch } from "../Hooks/useFetch"
 export const MultipleCustomHooks = () => {
 
 
-    const { getFetch, data, isLoading, hasError } = useFetch(' https://www.breakingbadapi.com/api/quotes/1 ')
+    const { data, isLoading, hasError } = useFetch(' https://www.breakingbadapi.com/api/quotes/1 ')
+    const { author, quote } = !!data && data[0];  // Esto lo que hace es que coge la posición 0 del array data si data no es nulo.
 
   return (
     <>
         <h1>BreakingBad Quotes</h1>
+        <hr />
+        {
+
+            isLoading
+                ? (
+                      <div className="alert alert-info text-center">
+                          Loading...
+                      </div>
+                )
+                : (
+                      <blockquote className="blockquote text-end">
+                          <p className="mb-1">{ quote } </p>
+                          <footer className="blockquote-footer"> { author } </footer>
+                      </blockquote>
+                )
+        }
+
+       
+
+   
     </>
   )
 }
