40f965e880ad17ad378d9eca11e5ffcfe332a600
Ana <amarcosestevez@gmail.com>
2022-08-22 13:59:01 +0200

    add two new components to simplify the principal component

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,5 +1,6 @@
-import { useFetch } from "../Hooks/useFetch"
-import { useCounter } from "../Hooks/useCounter";
+import { useFetch, useCounter } from "../Hooks";
+import { LoadingQuote, Quote } from "./";
+
 
 export const MultipleCustomHooks = () => {
 
@@ -15,17 +16,9 @@ export const MultipleCustomHooks = () => {
         {
 
             isLoading
-                ? (
-                      <div className="alert alert-info text-center">
-                          Loading...
-                      </div>
-                )
-                : (
-                      <blockquote className="blockquote text-end">
-                          <p className="mb-1">{ quote } </p>
-                          <footer className="blockquote-footer"> { author } </footer>
-                      </blockquote>
-                )
+                ? <LoadingQuote />
+                :  <Quote author={ author } quote={ quote }/>
+                
         }
 
        <button onClick={ onAdd } className="btn btn-primary" disabled={ isLoading }>
