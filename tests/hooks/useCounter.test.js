e6447d12b4354379ee5ea684e5c21ced06262418
Ana <amarcosestevez@gmail.com>
2022-08-24 12:43:59 +0200

    add first hooks's test

---
+++ tests/hooks/useCounter.test.js
@@ -0,0 +1,17 @@
+import { renderHook } from '@testing-library/react';
+import { useCounter } from '../../src/Hooks/useCounter';
+
+
+describe('Pruebas sobre el hook useCounter', () => { 
+
+    test('Debe de retornar los valores por defecto', () => { 
+
+        const { result } = renderHook( ()=> useCounter())
+        const { counter, onAdd, onReset, onSubtract } = result.current;
+
+        expect( counter ).toBe( 1 );
+        expect( onAdd ).toEqual( expect.any( Function )); //ESTO LO QUE DICE ES QUE ESPERA QUE onAdd SEA UNA FUNCION
+        expect( onReset ).toEqual( expect.any( Function ));
+        expect( onSubtract ).toEqual( expect.any( Function ));
+     })
+ })
