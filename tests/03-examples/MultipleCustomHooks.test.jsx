04d48f2c3c3cd073975f5982b8222293cfbdccb5
Ana <amarcosestevez@gmail.com>
2022-08-25 13:34:23 +0200

    add test to try the fetch return one quote

--- tests/03-examples/MultipleCustomHooks.test.jsx
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -1,15 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import { MultipleCustomHooks } from '../../src/03-examples';
+import { useFetch } from '../../src/Hooks/useFetch';
 
+ jest.mock('../../src/Hooks/useFetch');
 describe('Pruebas en el componente MultipleCustomHooks', () => { 
 
+  
+
     test('Debe de mostrar el componente por defecto', () => { 
+
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null,
+        })
+
        render( <MultipleCustomHooks /> );
-       screen.debug()
+
        expect(screen.getByText('Loading...'));
        expect(screen.getByText('BreakingBad Quotes'));
+
        const button = screen.getByRole('button', {name:'Next quote'})
        expect(button.disabled).toBeTruthy();
 
      })
+     test('Debe mostrar un quote', () => { 
+
+         useFetch.mockReturnValue({
+            data:[{ author:'Ana', quote:'Hoy es jueves'}],
+            isLoading: false,
+            hasError: null,
+        })
+        render( <MultipleCustomHooks/> );
+        screen.debug()
+        expect(screen.getByText('Ana'));
+        expect(screen.getByText('Hoy es jueves'));
+        
+        const button = screen.getByRole('button', { name:'Next quote'})
+        expect(button.disabled).toBeFalsy();
+      })
 })
