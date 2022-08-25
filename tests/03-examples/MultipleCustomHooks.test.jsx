4281ce421f0247313aba245a6fc2f1fe70512edc
Ana <amarcosestevez@gmail.com>
2022-08-25 12:56:38 +0200

    add first test to multipleCustomHook component

---
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { MultipleCustomHooks } from '../../src/03-examples';
+
+describe('Pruebas en el componente MultipleCustomHooks', () => { 
+
+    test('Debe de mostrar el componente por defecto', () => { 
+       render( <MultipleCustomHooks /> );
+       screen.debug()
+       expect(screen.getByText('Loading...'));
+       expect(screen.getByText('BreakingBad Quotes'));
+       const button = screen.getByRole('button', {name:'Next quote'})
+       expect(button.disabled).toBeTruthy();
+
+     })
+})
