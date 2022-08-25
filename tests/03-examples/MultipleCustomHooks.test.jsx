28f6da7a9d8c7c6ed549c95c411210dfee5c6d2b
Ana <amarcosestevez@gmail.com>
2022-08-25 14:13:11 +0200

    add test to try the function onAdd is called

--- tests/03-examples/MultipleCustomHooks.test.jsx
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -1,11 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, renderHook, fireEvent } from '@testing-library/react';
 import { MultipleCustomHooks } from '../../src/03-examples';
 import { useFetch } from '../../src/Hooks/useFetch';
+import { useCounter } from '../../src/Hooks';
 
  jest.mock('../../src/Hooks/useFetch');
+ jest.mock('../../src/Hooks/useCounter');
 describe('Pruebas en el componente MultipleCustomHooks', () => { 
 
-  
+    const mockOnAdd = jest.fn();
+    useCounter.mockReturnValue({
+       counter:1,
+       onAdd:mockOnAdd,
+   })
+
+   beforeEach(()=>
+   jest.clearAllMocks())
+   
 
     test('Debe de mostrar el componente por defecto', () => { 
 
@@ -32,11 +42,37 @@ describe('Pruebas en el componente MultipleCustomHooks', () => {
             hasError: null,
         })
         render( <MultipleCustomHooks/> );
-        screen.debug()
+
         expect(screen.getByText('Ana'));
         expect(screen.getByText('Hoy es jueves'));
         
         const button = screen.getByRole('button', { name:'Next quote'})
         expect(button.disabled).toBeFalsy();
       })
+
+      test('Debe de llamar la función de incrementar', () => { 
+
+     
+
+        useFetch.mockReturnValue({
+            data:[{ author:'Ana', quote:'Hoy es jueves'}],
+            isLoading: false,
+            hasError: null,
+        })
+      
+       
+        render( <MultipleCustomHooks/> );
+
+        const button = screen.getByRole('button', {name:'Next quote'})
+       
+
+        fireEvent.click( button );
+        
+      
+       
+        expect( mockOnAdd ).toHaveBeenCalled();
+
+        
+
+       })
 })
